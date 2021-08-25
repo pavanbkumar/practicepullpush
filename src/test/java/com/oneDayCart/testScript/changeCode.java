963bdadcd501125026be376db57076da4d229934
pavanbkumar <pavankumarb16196@gmail.com>
2021-08-25 14:27:39 +0530

    This is commit from new wok space

---
+++ src/test/java/com/oneDayCart/testScript/changeCode.java
@@ -0,0 +1,13 @@
+package com.oneDayCart.testScript;
+
+import org.testng.annotations.Test;
+
+public class changeCode {
+
+	
+	
+	@Test
+	public void updateMethod(){
+		System.out.println("Hello world");
+	}
+}
---
+++ src/test/java/com/oneDayCart/testScript/changeUpdate.java
@@ -0,0 +1,13 @@
+package com.oneDayCart.testScript;
+
+import org.testng.annotations.Test;
+
+public class changeUpdate {
+
+	
+	
+	@Test
+	public void check(){
+		System.out.println("How are you");
+	}
+}
